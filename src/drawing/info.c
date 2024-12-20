6cf93e592c91b10ff812c9dc37e3fa0d7a7d5ef9
Charles POULAIN <charles.probe@gmail.com>
2024-12-20 13:06:59 +0100

    ~ | Fixed leaks.

--- src/drawing/info.c
+++ src/drawing/info.c
@@ -6,7 +6,7 @@
 /*   By: cpoulain <cpoulain@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:00:30 by cpoulain          #+#    #+#             */
-/*   Updated: 2024/12/20 10:44:28 by cpoulain         ###   ########.fr       */
+/*   Updated: 2024/12/20 13:00:23 by cpoulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,26 +32,29 @@ void	ft_info_cam(
 	t_fdf *fdf
 )
 {
+	char	*str;
+
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 58}, INF_COLOR, "---- CAM ----");
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 70}, INF_COLOR, "Depth :");
-	ft_mlx_string_put(fdf, (t_2d_vector){90, 70},
-		INF_COLOR, ft_dtoa(fdf->camera.fov, 1));
+	str = ft_dtoa(fdf->camera.fov, 1);
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 70}, INF_COLOR, str);
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 82}, INF_COLOR, "Offset :");
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 94}, INF_COLOR, "  X :");
-	ft_mlx_string_put(fdf, (t_2d_vector){90, 94},
-		INF_COLOR, ft_dtoa(fdf->camera.offset.x, 2));
+	(free(str), str = ft_dtoa(fdf->camera.offset.x, 2));
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 94}, INF_COLOR, str);
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 106}, INF_COLOR, "  Y :");
-	ft_mlx_string_put(fdf, (t_2d_vector){90, 106},
-		INF_COLOR, ft_dtoa(fdf->camera.offset.y, 2));
+	(free(str), str = ft_dtoa(fdf->camera.offset.y, 2));
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 106}, INF_COLOR, str);
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 118}, INF_COLOR, "rX :");
-	ft_mlx_string_put(fdf, (t_2d_vector){90, 118},
-		INF_COLOR, ft_dtoa(fdf->camera.x_deg * 57.2958, 5));
+	(free(str), str = ft_dtoa(fdf->camera.x_deg * 57.2958, 5));
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 118}, INF_COLOR, str);
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 130}, INF_COLOR, "rY :");
-	ft_mlx_string_put(fdf, (t_2d_vector){90, 130},
-		INF_COLOR, ft_dtoa(fdf->camera.y_deg * 57.2958, 5));
+	(free(str), str = ft_dtoa(fdf->camera.y_deg * 57.2958, 5));
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 130}, INF_COLOR, str);
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 142}, INF_COLOR, "rZ :");
-	ft_mlx_string_put(fdf, (t_2d_vector){90, 142},
-		INF_COLOR, ft_dtoa(fdf->camera.z_deg, 2));
+	(free(str), str = ft_dtoa(fdf->camera.z_deg * 57.2958, 5));
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 142}, INF_COLOR, str);
+	free(str);
 }
 
 void	ft_info(
@@ -59,6 +62,7 @@ void	ft_info(
 )
 {
 	char	proj_mode[2];
+	char	*str;
 
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 10}, INF_COLOR, "---- FDF ----");
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 22}, INF_COLOR, "Projection :");
@@ -66,7 +70,8 @@ void	ft_info(
 	proj_mode[0] = fdf->projection_mode;
 	ft_mlx_string_put(fdf, (t_2d_vector){90, 22}, INF_COLOR, proj_mode);
 	ft_mlx_string_put(fdf, (t_2d_vector){10, 34}, INF_COLOR, "Scale :");
-	ft_mlx_string_put(fdf,
-		(t_2d_vector){90, 34}, INF_COLOR, ft_dtoa(fdf->scale, 0));
+	str = ft_itoa((int)fdf->scale);
+	ft_mlx_string_put(fdf, (t_2d_vector){90, 34}, INF_COLOR, str);
+	free(str);
 	ft_info_cam(fdf);
 }
--- src/utils/free_fdf.c
+++ src/utils/free_fdf.c
@@ -6,7 +6,7 @@
 /*   By: cpoulain <cpoulain@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:43:11 by cpoulain          #+#    #+#             */
-/*   Updated: 2024/12/10 14:59:33 by cpoulain         ###   ########.fr       */
+/*   Updated: 2024/12/20 13:05:47 by cpoulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	free_fdf(
 		ft_free_2d_map_elements(&(fdf->map));
 	if (fdf->mlx_data.mlx)
 	{
+		if (fdf->mlx_data.img_data.img)
+			mlx_destroy_image(fdf->mlx_data.mlx, fdf->mlx_data.img_data.img);
 		if (fdf->mlx_data.window)
 			mlx_destroy_window(fdf->mlx_data.mlx, fdf->mlx_data.window);
 		mlx_destroy_display(fdf->mlx_data.mlx);
